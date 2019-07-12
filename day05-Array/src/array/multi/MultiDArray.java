89c4db1aaa0118f7423c2a6c86b9ae3fcfb2dcb4
imlimchill <cojwd300@gmail.com>
2019-07-12 10:09:36 +0900

    다차원 배열로 계단별 찍기

--- day05-Array/src/array/multi/MultiDArray.java
+++ day05-Array/src/array/multi/MultiDArray.java
@@ -12,17 +12,25 @@ public class MultiDArray {
 		// 별 입력
 		for (int idx = 0; idx < stars.length; idx++) {
 			stars[idx] = new char[idx + 1];
-			for (int ind = 0; idx < stars[idx].length; ind++) {
+			for (int ind = 0; ind < stars[idx].length; ind++) {
 				stars[idx][ind] = '☆';
 			}
 		}
 		
 		// 별 출력
-		for (char[] star: stars) {
-			for(char st: star) {
-				System.out.println(st);
+		for (int idx = 0; idx < stars.length; idx++) {
+			for (int ind = 0; ind < stars[idx].length; ind++) {
+				System.out.printf("%c\t", stars[idx][ind]);
 			}
+			System.out.println();
 		}
+		
+		
+//		for (char[] star: stars) {
+//			for(char st: star) {
+//				System.out.println(st);
+//			}
+//		}
 		
 	} // end main
 
