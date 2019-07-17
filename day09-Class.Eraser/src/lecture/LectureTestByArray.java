2eec796bfe2d5cdea90321afe00aaebd5ce8dac3
imlimchill <cojwd300@gmail.com>
2019-07-17 14:50:42 +0900

    마커 지우개 클래스 배열을 통해 사용하기

--- day09-Class.Eraser/src/lecture/LectureTestByArray.java
+++ day09-Class.Eraser/src/lecture/LectureTestByArray.java
@@ -1,14 +1,30 @@
 package lecture;
 
 /**
- * 지우개, 마커 선언해
- * 값을 배열에 집어넣어
- * 사용 : loop 돌려서 한번씩 사용
- *        지우개 돌려 loop
- *        양쪽 지우개 한장씩 제거 loop
- *        마커의 상태출력 loop
- *        지우개 상태출력 loop
- * @author Administrator
+ * 2개의 지우개와 4개의 마커를 
+ * 배열을 사용하여 사용/관리 하는 테스트 클래스
+ * --------------------------------------------
+ * 1. 마커(Marker) 배열 markers 를 선언
+ *    지우개(Eraser) 배열 erasers 를 선언
+ *    
+ * 2. markers 는 길이 4로 생성
+ *    eraser 는 길이 2로 생성
+ *  
+ *  markers[0] <== 에는 순서대로 
+ *  ~ 
+ *  markers[3] red, green, blue, black 마커를 저장 
+ *  
+ *  erasers[0], [1] 에는 순서대로
+ *            leftEraser, rightEraser 를 저장
+ *            
+ * 3. foreach 를 사용하여
+ *   (1) 모든 마커를 한번씩 사용(use())
+ *   (2) 모든 지우개를 한번씩 사용(erase())
+ *   (3) 모든 지우개를 한번씩 한 겹 벗겨내기(peel())
+ *   (4) 모든 지우개의 상태 출력
+ *   (5) 모든 마커의 상태 출력          
+ *  
+ * @author 
  *
  */
 public class LectureTestByArray {
