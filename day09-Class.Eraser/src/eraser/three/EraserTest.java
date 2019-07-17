3ec2968c5cf37403ccab59fac6fb6fe2808efd75
imlimchill <cojwd300@gmail.com>
2019-07-17 11:20:12 +0900

    this() 사용하기

---
+++ day09-Class.Eraser/src/eraser/three/EraserTest.java
@@ -0,0 +1,45 @@
+package eraser.three;
+
+/**
+ * 칠판 지우개를 만들고 사용해보는 클래스
+ * @author Administrator
+ *
+ */
+public class EraserTest {
+
+	public static void main(String[] args) {
+		// 1. 선언 : 칠판지우개 객체 참조변수
+		Eraser leftEraser;
+		Eraser rightEraser;
+		
+		// 2. 초기화
+		// 기본 생성자로 생성
+		leftEraser = new Eraser();
+		// 매개변수가 있는 생성자로 생성
+		rightEraser = new Eraser(3.5, 12.0, 5.5, 3, "흰색");
+		
+		// 3. 사용
+		// (1) 초기상태
+		leftEraser.print();
+		rightEraser.print();
+		
+		// (2) 오른쪽 한겹 제거
+		rightEraser.peel();
+		
+		// (3) 두 지우개 상태 출력
+		leftEraser.print();
+		rightEraser.print();
+		
+		// (4) 둘 다 한겹 제거
+		leftEraser.peel();
+		rightEraser.peel();
+		
+		// (5) 왼쪽 칠판지우기
+		leftEraser.erase();
+		
+		// (6) 두 지우개 상태 출력
+		leftEraser.print();
+		rightEraser.print();
+	}
+
+}
